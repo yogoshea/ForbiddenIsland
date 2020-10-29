f860b4c5e7b47155d2297888bc5b6b30e8a88fce
yogoshea <eoghanoshea0@gmail.com>
2020-10-29 16:54:17 +0000

    Adding Game and GamePlay classes to begin implementation

---
+++ src/island/game/Game.java
@@ -0,0 +1,10 @@
+package island.game;
+
+/**
+ * Game class describes in full the status of the game
+ * @author Eoghan O'Shea and Robert McCarthy
+ *
+ */
+public class Game {
+
+}
---
+++ src/island/main/GamePlay.java
@@ -0,0 +1,10 @@
+package island.main;
+
+/**
+ * GamePlay class controls the game events at the highest level
+ * @author Eoghan O'Shea and Robert McCarthy
+ *
+ */
+public class GamePlay {
+
+}
